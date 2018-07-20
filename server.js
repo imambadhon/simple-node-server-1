f90d493daf490f3d639ed7c57ab89a0fb5130633
Shama Hoque <shama.hoque@gmail.com>
2018-07-21 00:38:26 +0600

    cw 7

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 var server = http.Server(app);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
-})
+});
 
-app.get('/about', function(req, res){
+app.get('/form', function(req, res){
+  res.sendFile(__dirname+'/form.html');
+});
+
+app.post('/submit_user', function(req, res){
+  console.log(req.body);
+});
+
+app.get('/system/about', function(req, res){
   res.sendFile(__dirname+'/about.html');
 });
 
