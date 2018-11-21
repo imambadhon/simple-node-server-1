719de3ed976016d5d4d4e07babd2f0297a2fbfa5
Shama Hoque <shama.hoque@gmail.com>
2018-11-22 01:04:20 +0600

    cw9a

--- server.js
+++ server.js
@@ -4,6 +4,30 @@ var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+
+//for c9
+var db;
+//var db_url = "mongodb://"+process.env.IP+":27017"
+var db_url = "mongodb://localhost:27017"
+
+mongo.MongoClient.connect(db_url,
+  {useNewUrlParser:true}, function(err, client){
+    if(err){
+      console.log('Could not connect to MongoDB');
+    }else{
+      db = client.db('node-cw9');
+    }
+  })
+
+var save = function(form_data){
+  db.createCollection('articles', function(err, collection){
+
+  });
+  var collection = db.collection('articles');
+  collection.save(form_data);
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -27,7 +51,8 @@ app.post('/article/create', function(request, response){
     return response.status(400)
                     .json({error: "Please add a title"});
   }
-  article.push(request.body)
+  // article.push(request.body);
+  save(request.body)
   return response.status(200)
                 .json({message: "Article successfully created"});
 });
