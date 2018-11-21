e0710b0d2f0d180a96505996a7131b046757715c
Shama Hoque <shama.hoque@gmail.com>
2018-11-22 00:40:17 +0600

    cw8b

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var http = require('http');
 var express =require('express');
 var app = express();
 var server = http.Server(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(request, response){
   response.sendFile(__dirname+'/index.html');
@@ -9,6 +13,23 @@ app.get('/', function(request, response){
 
 app.get('/about-page', function(request, response){
   response.sendFile(__dirname+'/about.html');
+});
+
+app.get('/new-article', function(request, response){
+  response.sendFile(__dirname+'/form.html');
+});
+
+var article = [];
+
+app.post('/article/create', function(request, response){
+  console.log(request.body);
+  if(!request.body.title){
+    return response.status(400)
+                    .json({error: "Please add a title"});
+  }
+  article.push(request.body)
+  return response.status(200)
+                .json({message: "Article successfully created"});
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function(){
